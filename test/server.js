2f73f2526b5dc1969d067ffdf7af7fe3c8bb6d48
Elena <elena.vilimaite@gmail.com>
2016-06-27 15:38:22 +0100

    testing suite added

---
+++ test/server.js
@@ -0,0 +1,18 @@
+var expect  = require("chai").expect;
+var request = require("request");
+
+describe("Binary BnB", function() {
+
+  describe("There is a server", function() {
+
+    var url = "http://localhost:3000/";
+
+    it("returns status 200", function() {
+      request(url, function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+      });
+    });
+
+  });
+
+});
