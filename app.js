f93486f583891fade43c857d137d3a8db19a24e8
Elena <elena.vilimaite@gmail.com>
2016-06-28 16:02:48 +0100

    no need for loading .env at the moment

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var r = require('rethinkdbdash')();
-require('dotenv').load();
-
 
 // Need to include ORM models into app.js to work with routes
 
