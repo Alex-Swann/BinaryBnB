cbffb6d1a6b6ef0628045d43c08337c52d8ed9a6
Alex-Swann <alex_swann@hotmail.co.uk>
2016-06-29 11:25:51 +0100

    added unit test for User model

--- test/userSpec.js
+++ test/userSpec.js
@@ -1,16 +1,31 @@
+var Browser = require('zombie');
 var expect  = require("chai").expect;
 var request = require("request");
 require('./globalBefore');
+var user = require('../models/User');
+
+Browser.localhost('localhost:', 3000);
 
 describe('User', function(){
-  var user;
 
-  beforeEach(function(){
-      user = new User();
+  var browser = new Browser();
+
+  before(function(done) {
+    browser.visit('/', done);
   });
 
-  it('should save a new user', function(){
+  before(function(done) {
+      browser
+        .fill('name', 'Al')
+        .fill('username', 'alex427')
+        .fill('email',    'zombie@underworld.dead')
+        .fill('password', 'eat-the-living')
+        .pressButton('Submit', done);
+  });
 
+
+  it('should save a new user', function(){
+    
   });
 
 });
