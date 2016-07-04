6d847a13ddc9d5f1a651f286b441aac3dc47e736
Elena <elena.vilimaite@gmail.com>
2016-07-04 11:19:50 +0100

    it's the simple things that work

--- routes/requests.js
+++ routes/requests.js
@@ -4,7 +4,7 @@ var request = require('../models/Request');
 var space = require('../models/Space');
 
 router.post('/', function(req, res, next){
-  res.render('requests/new', { title: 'Requests', spaceId: req.body.spaceId,  userId: req.body.userId})
+  res.render('requests/new', { title: 'Make a new Request', spaceId: req.body.spaceId,  userId: req.body.userId})
 });
 
 router.post('/new', function(req, res, next){
